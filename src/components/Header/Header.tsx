616423beceb712685e8df4dd5eb9b1255e1d443e
Bachicada <bachicada@gmail.com>
2022-05-06 16:59:01 +0300

    feat: add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+import "./Header.scss";
+
+export const Header = () => {
+  const [isSticky, setSticky] = useState(false);
+  const headerLine = useRef<HTMLElement>(null);
+
+  const checkSticky = () => {
+    if (
+      headerLine.current?.clientHeight &&
+      window.pageYOffset > headerLine.current?.clientHeight
+    ) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkSticky);
+    return () => {
+      window.removeEventListener("scroll", checkSticky);
+    };
+  });
+  return (
+    <header ref={headerLine} className={isSticky ? "header sticky" : "header"}>
+      <h1 className="h1">Project Management App</h1>
+
+      <div className="switch">
+        <input
+          id="language-toggle"
+          className="check-toggle check-toggle-round-flat"
+          type="checkbox"
+        />
+        <label htmlFor="language-toggle"></label>
+        <span className="on">RU</span>
+        <span className="off">EN</span>
+      </div>
+      <button className="headerBtn">+ create new board</button>
+      <button className="headerBtn">Log In</button>
+    </header>
+  );
+};

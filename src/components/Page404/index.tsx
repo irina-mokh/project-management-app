cd7021ce6dd7bfd5ce7eabb99e3b167b6c1e6ba3
Bachicada <bachicada@gmail.com>
2022-05-09 14:18:36 +0300

    feat: add page404

---
+++ src/components/Page404/index.tsx
@@ -0,0 +1,20 @@
+import "./Page404.scss";
+export const Page404 = () => {
+  return (
+    <div className="container">
+      <h1 className="title404">Простите, страница не найдена</h1>
+      <div className="col-md-12">
+        <div className="a-one animate"></div>
+        <div className="a-two animate"></div>
+        <div className="a-three animate"></div>
+        <div className="a-four animate"></div>
+        <div className="a-five animate"></div>
+        <div className="a-six animate"></div>
+        <div className="a-seven animate"></div>
+        <div className="a-eight animate"></div>
+        <div className="a-nine animate"></div>
+        <div className="a-ten animate"></div>
+      </div>
+    </div>
+  );
+};

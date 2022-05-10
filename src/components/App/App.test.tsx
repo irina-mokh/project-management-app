68c6523eb04f1632a749cbb450d65d67c138a8c8
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-05-10 22:53:47 +0300

    refactor: prettier fixes

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from ".";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '.';
 
-test("renders App", () => {
+test('renders App', () => {
   render(<App />);
-  const el = screen.getByTestId("app");
+  const el = screen.getByTestId('app');
   expect(el).toBeInTheDocument();
 });

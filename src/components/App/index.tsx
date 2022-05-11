6f619e84a35f240582de1f38b7fdbc74a2392346
karlsonru <ivpolovnikov@hotmail.com>
2022-05-12 01:14:32 +0300

    feat: add component initial layout

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from 'routes';
 
+import { NewBoardModal } from 'components/NewBoardModal';
+
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <div data-testid="app">
           <AppRouter />
+          <NewBoardModal />
         </div>
       </ThemeProvider>
     </BrowserRouter>

559d0effc87fca6d5a8f5f9c841899a25bd046d6
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-05-26 19:29:41 +0300

    refactor: signin test

--- src/pages/signIn/signIn.test.tsx
+++ src/pages/signIn/signIn.test.tsx
@@ -4,29 +4,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from 'store';
 
-// const setup = () => {
-//   render(
-//     <MemoryRouter initialEntries={['/signin']}>
-//       <Provider store={store}>
-//         <AppRouter />
-//       </Provider>
-//     </MemoryRouter>
-//   );
-
-//   const form = screen.getByTestId('signIn');
-//   const login = screen.getByLabelText(/login/i);
-//   const password = screen.getByLabelText(/password/i);
-//   const submit = screen.getByRole('button');
-
-//   return {
-//     form,
-//     login,
-//     password,
-//     submit,
-//   };
-// };
-
-test('Signin', async () => {
+const setup = () => {
   render(
     <MemoryRouter initialEntries={['/signin']}>
       <Provider store={store}>
@@ -39,16 +17,31 @@ test('Signin', async () => {
   const login = screen.getByLabelText(/login/i);
   const password = screen.getByLabelText(/password/i);
 
-  expect(page).toBeInTheDocument();
-  expect(login).toBeInTheDocument();
-  expect(password).toBeInTheDocument();
+  return {
+    page,
+    login,
+    password,
+  };
+};
 
-  fireEvent.change(login, { target: { value: 'mokh' } });
-  fireEvent.change(password, { target: { value: '007007pswrd' } });
-  const submit = screen.getByTestId('submit-signin');
-  expect(submit).toBeInTheDocument();
+describe('Signin', () => {
+  it('render', async () => {
+    const { login, page, password } = setup();
 
-  fireEvent.click(submit);
+    expect(page).toBeInTheDocument();
+    expect(login).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+  });
+  it('fill and submit', async () => {
+    const { login, password } = setup();
 
-  await waitFor(() => expect(screen.getByText('Welcome page')).toBeInTheDocument());
+    fireEvent.change(login, { target: { value: 'mokh' } });
+    fireEvent.change(password, { target: { value: '007007pswrd' } });
+    const submit = screen.getByTestId('submit-signin');
+    expect(submit).toBeInTheDocument();
+
+    fireEvent.click(submit);
+
+    await waitFor(() => expect(screen.getByText('Welcome page')).toBeInTheDocument());
+  });
 });
